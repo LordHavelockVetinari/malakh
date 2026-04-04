4ad0f4b1ba50c22088fc9475c22db6132e648b7d
LordHavelockVetinari <42384284+LordHavelockVetinari@users.noreply.github.com>
2026-04-04 09:14:15 +0300

    Added negative tests.

--- test/test.py
+++ test/test.py
@@ -5,6 +5,9 @@ import textwrap
 from threading import Thread, Lock
 import queue
 from queue import Queue
+from collections import namedtuple
+
+Job = namedtuple("Job", ["dir", "name"])
 
 EXECUTABLE = os.path.join(
     os.path.dirname(os.path.dirname(__file__)),
@@ -27,20 +30,30 @@ job_queue = Queue()
 def print_indented(b: bytes):
     print(textwrap.indent(b.decode("utf-8", errors="replace"), " " * 4))
 
-def do_job(name):
-    malfile = name + ".mal"
-    input = read(name + ".in")
-    output = read(name + ".out")
-    error = read(name + ".err")
+dir_options = {
+    "success": ["--raw-errors"],
+    "compilation_error": ["--check"]
+}
+
+def do_job(job):
+    malfile = os.path.join(job.dir, job.name + ".mal")
+    input = read(os.path.join(job.dir, job.name + ".in"))
+    output = read(os.path.join(job.dir, job.name + ".out"))
+    error = read(os.path.join(job.dir, job.name + ".err"))
     completed = subprocess.run(
-        [EXECUTABLE, malfile, "--raw-errors"],
+        [EXECUTABLE, malfile, *dir_options[job.dir]],
         input=input,
         stdout=PIPE,
         stderr=PIPE,
     )
     with lock:
-        if completed.stderr != error:
-            print("ERROR: test", repr(name), "returned wrong stderr")
+        if job.dir == "compilation_error" and completed.returncode != 1:
+            if completed.returncode == 0:
+                print("ERROR: test", malfile, "unexpectedly compiled")
+            else:
+                print("ERROR: test", malfile, "compilation failed with exit code", completed.returncode)
+        elif job.dir == "success" and completed.stderr != error:
+            print("ERROR: test", malfile, "returned wrong stderr")
             if error == b"":
                 print("EXPECTED: no error")
             else:
@@ -48,8 +61,8 @@ def do_job(name):
                 print_indented(error)
             print("GOT:")
             print_indented(completed.stderr)
-        elif completed.stdout != output:
-            print("ERROR: test", repr(name), "returned wrong output")
+        elif job.dir == "success" and completed.stdout != output:
+            print("ERROR: test", malfile, "returned wrong output")
             if output == b"":
                 print("EXPECTED: no output.")
             else:
@@ -58,7 +71,7 @@ def do_job(name):
             print("GOT:")
             print_indented(completed.stdout)
         else:
-            print("OK: test", repr(name), "passed")
+            print("OK: test", malfile, "passed")
             global num_successful
             num_successful = num_successful + 1
 
@@ -75,19 +88,21 @@ def main():
     global num_successful
     num_tests = num_successful = 0
     subprocess.run("cargo build", check=True, stderr=DEVNULL)
-    dir = os.path.join(os.path.dirname(__file__), "success")
-    os.chdir(dir)
-    for malfile in os.listdir(dir):
-        name, ext = os.path.splitext(malfile)
-        if ext != ".mal":
-            continue
-        num_tests += 1
-        job_queue.put(name)
+
+    os.chdir(os.path.dirname(__file__))
+    for dir in ["success", "compilation_error"]:
+        for malfile in os.listdir(dir):
+            name, ext = os.path.splitext(malfile)
+            if ext != ".mal":
+                continue
+            num_tests += 1
+            job_queue.put(Job(dir, name))
+
     job_queue.shutdown()
     for i in range(os.cpu_count()):
         Thread(target=worker).start()
     job_queue.join()
-    
+
     print()
     if num_successful == num_tests:
         print(f"All {num_tests} tests passed!")
